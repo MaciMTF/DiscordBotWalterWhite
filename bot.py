bc24786fe1e4c0c58aa20a5225081a95f82c0c2c
Maci <89538371+MrMacio@users.noreply.github.com>
2022-10-09 14:37:10 +0200

    www

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from nextcord.ext import commands
+
+def main():
+    client = commands.Bot(command_prefix="?")
+
+    load_dotenv()
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user.name} has connected to Discord.")
+
+    # load all cogs
+    for folder in os.listdir("modules"):
+        if os.path.exists(os.path.join("modules", folder, "cog.py")):
+            client.load_extension(f"modules.{folder}.cog")
+
+    client.run(os.getenv("DISCORD_TOKEN"))
+
+if __name__ == '__main__':
+    main()
